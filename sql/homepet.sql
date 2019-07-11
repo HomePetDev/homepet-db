743b4810134a72c296cde4cea4d58772abf3876a
unknown <felixlopezarm1@gmail.com>
2019-07-11 09:45:50 -0400

    :heavy_plus_sign: Added some razas , modified some varchar values, added column fotoUrl to raza table

--- sql/homepet.sql
+++ sql/homepet.sql
@@ -52,14 +52,14 @@ CREATE TABLE  hmpet.almacen(
 CREATE TABLE  hmpet.raza(
   nombre_especie varchar(15)references hmpet.animal(nombre_especie),
   nombre_raza varchar(15),
-  descripcion varchar(100) NOT NULL ,
+  descripcion varchar(255) NOT NULL ,
   pais varchar(15) NOT NULL ,
-  iq int NOT NULL ,
-  contextura varchar(20) NOT NULL ,
-  talla varchar(10) NOT NULL CHECK (talla IN ('S','M','L')),
-  color varchar(20) NOT NULL ,
-  altura varchar(100) NOT NULL ,
-  peso varchar(100) NOT NULL ,
+  iq varchar(255) NOT NULL ,
+  talla varchar(10) NOT NULL CHECK (talla IN ('S','M','L', 'XL')),
+  color varchar(255) NOT NULL ,
+  altura varchar(255) NOT NULL ,
+  peso varchar(255) NOT NULL ,
+  fotoUrl varchar(255),
 
   PRIMARY KEY(nombre_especie,nombre_raza)
 );
@@ -198,7 +198,7 @@ CREATE TABLE  hmpet.modos_pago(
 CREATE TABLE  hmpet.productos(
   id_producto varchar(10),
   nombre_prod varchar(35) NOT NULL UNIQUE ,
-  descripcion varchar(200) NOT NULL ,
+  descripcion varchar(200) NOT NULL , 
   precio int NOT NULL CHECK(precio>=0),
   instrucciones varchar(200)NOT NULL ,
   nombre_especie varchar(15) NOT NULL references hmpet.animal(nombre_especie),
@@ -209,7 +209,7 @@ CREATE TABLE  hmpet.productos(
 
 CREATE TABLE  hmpet.comidas(
   id_comida varchar(10) references hmpet.productos(id_producto),
-  talla varchar(10) NOT NULL CHECK (talla IN ('S','M','L')),
+  talla varchar(10) NOT NULL CHECK (talla IN ('S','M','L', 'XL')),
 
   PRIMARY KEY(id_comida)
 );
@@ -397,18 +397,78 @@ CREATE TABLE  hmpet.comida_porciondiaria_peso(
 );
 
 
+-- Típos de usuario
 INSERT INTO hmpet.accesos VALUES ('1', 'usuario');
 INSERT INTO hmpet.accesos VALUES ('2', 'cliente');
 INSERT INTO hmpet.accesos VALUES ('3', 'empleado');
 INSERT INTO hmpet.accesos VALUES ('4', 'gerente');
 
+-- Animales
 INSERT INTO hmpet.animal VALUES ('perro', 'Animal de cuatro patas amigable y confiable');
 INSERT INTO hmpet.animal VALUES ('gato', 'Animal de cuatro patas un poco mas odioso');
 INSERT INTO hmpet.animal VALUES ('hamster', 'Pequeño peludo y muy jugueton');
 INSERT INTO hmpet.animal VALUES ('delfin', 'Animal acuatico muy inteligente');
 
+-- Modos de pago
 INSERT INTO hmpet.modos_pago VALUES ('efectivo');
 INSERT INTO hmpet.modos_pago VALUES ('cheque');
 INSERT INTO hmpet.modos_pago VALUES ('tarjeta debito');
 INSERT INTO hmpet.modos_pago VALUES ('tarjeta credito');
 INSERT INTO hmpet.modos_pago VALUES ('transferencia');
+
+-- Razas
+-- Perros
+INSERT INTO hmpet.raza VALUES ('perro', 'mastin español', 
+  'Un perro de gran talla, muy potente y robusto, pero a la vez es flexible y ágil. Le caracteriza su cabeza grande (pero proporcionada) y su papada. De ojos pequeños (en relación al craneo) y orejas medianas y caidas.',
+  'españa', 'gran inteligencia' , 'XL' , 'color hay una gran variedad, amarillos, negros, leonados, cervatos... pueden ser uniformes (suelen ser los más apreciados) o combinados.' , 'Entre 72 y 88 cm', 'Entre 50 a 120 kg' , 
+  'https://www.terranea.es/assets/images/razas/mastin_espanol2.jpg'
+);
+
+INSERT INTO hmpet.raza VALUES ('perro', 'san bernardo', 
+  'Un perro de gran talla, muy potente y robusto, pero a la vez es flexible y ágil.',
+  'suiza', 'gran inteligencia' , 'XL' , 'Blanco con grandes manchas de color marrón-rojizo.' , 'Entre 70 y 90 cm', 'Entre 55 a 90 Kg', 'http://www.todoperro.es/wallpapers/p-bernardo.jpg'
+);
+
+
+INSERT INTO hmpet.raza VALUES ('perro', 'gran danes', 
+  'El Gran Danes es un excelente perro de guardia a la vez que perro de compañia. Muy cariñoso y fiel con sus dueños, especialmente si se trata de niños',
+  'alemania', 'gran inteligencia' , 'XL' , 'leonado (desde colores pálidos hasta dorados intensos), atigrado, arlequín (parches negros y blancos), negro azabache y azul acero oscuro.' , 'Entre 72 y 80 cm', 'Entre 55 a 80 Kg',
+  'http://www.todoperro.es/wallpapers/p-grandanes.jpg'
+);
+
+INSERT INTO hmpet.raza VALUES ('perro', 'rottweiler', 
+  'Es una raza grande, de aspecto robusto, fuerte, bien proporcionado y compacto. De hocico profundo y amplio, con la trufa negra. Orejas caidas, pequeñas y con forma triangular.',
+  'alemania', 'gran inteligencia' , 'L' , 'Son de negros con marcas pardo-rojizas bien delimitadas y situadas bajo la cola, en hocico, mejillas, garganta, pecho y extremidades principalmente.' , 'Entre 56 y 68 cm', 'Entre 55 a 80 Kg'
+  'http://www.todoperro.es/fotos/originales/p8840.jpg'
+);
+
+INSERT INTO hmpet.raza VALUES ('perro', 'pastor aleman', 
+  'El Pastor Alemán es una de las razas más populares, excelente trabajador, guardián, afectuoso y paciente con los más pequeños de la familia.',
+  'alemania', 'gran inteligencia' , 'L' , 'Negro con marcas de colores marrón, amarillo, gris claro o café-rojizo. Pelaje doble con lanilla interna.' , 'Entre 55 y 65 cm', 'Entre 22 a 40 Kg',
+  'http://www.todoperro.es/fotos/originales/p27162.jpg'
+);
+
+INSERT INTO hmpet.raza VALUES ('perro', 'bulldog ingles', 
+  'Son perros que destacan por su valentía y su habilidad como guardiantes, siempre alerta. Su feroz apariencia esconde en realidad a un perro afectivo, leal y muy dependiente.',
+  'gran bretaña', 'gran inteligencia' , 'M' , 'Pueden ser de un solo color, atigrados, leonados, con distintos tonos de rojo... Son bastante comunes los blancos combinados con otro color.' , 'Entre 30 y 40 cm', 'Entre 20 a 25 Kg',
+  'http://www.todoperro.es/fotos/originales/p12820.jpg'
+);
+
+
+INSERT INTO hmpet.raza VALUES ('perro', 'dalmata', 
+  'Se trata de perros amistosos y muy sensibles que necesitan la compañía del ser humano. Es necesario una buena socialización para evitar que se conviertan en timidos y reservados',
+  'croacia', 'gran inteligencia' , 'M' , 'Blanco con manchas de color negro o hígado-marrón (son dos variedades, no mezcladas).' , 'Entre 54 y 60 cm', 'Entre 24 a 27 Kg',
+  'https://www.curiosfera.com/wp-content/uploads/2018/04/dalmata.jpg'
+);
+
+INSERT INTO hmpet.raza VALUES ('perro', 'bulldog frances', 
+  'Un perro de tamaño pequeño, musculoso y muy robusto, de aspecto regorderte. Destaca su hocico corto, las orejas ergidas y su nariz ancha muy corta y negra.',
+  'francia', 'promedio' , 'S' , 'son de tono leonado uniforme con o sin parches blancos. Las manchas blancas pueden ser desde poco extendidas hasta completamente blancos.' , 'Entre 20 y 30 cm', 'Entre 8 a 14 Kg',
+  'https://www.purina.es/sites/g/files/mcldtz1656/files/2019-06/frenchbull_400x378_0.jpg'
+);
+
+INSERT INTO hmpet.raza VALUES ('perro', 'chihuahua', 
+  ' son mejor conocidos por su tamaño pequeño y sus largas y rectas orejas y su pelaje corto.',
+  'mexico', 'promedio' , 'S' , 'negro sólido hasta blanco sólido, manchados, o en una variedad de colores como cervatillo (café), chocolate, azul (gris), plata, tricolores (chocolate, azul, o negro y blanco con marcas cafés y blancas), con líneas, y moteados. ' , 'Entre 16 y 20 cm', 'Entre 500g a 3 Kg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOitnc2F7uyE72eaxORqISdYhHo7y6Dit2YBS4q1AKqhRl2yKcRw'
+);
